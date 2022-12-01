177eae4d4ae77bd73fd81af7ef8ae74cf88ffedb
igor02b1 <igor02b1@gmail.com>
2022-11-30 23:55:23 -0300

    feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
-import './App.css';
-import Navbar from './componentes/Navbar/navbar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './componentes/estaticos/Navbar/navbar'
+import { Contato } from './paginas/Contato/Contato';
+import { Home } from './paginas/Home/Home';
+import { Preços } from './paginas/Preços/Preços'
+import { Sobre } from './paginas/sobre/Sobre';
+
 
 function App() {
-
   return (
-    <>
+    <Router>
       <Navbar />
-
-    </>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/preços' element={<Preços />} />
+        <Route path='/contato' element={<Contato />} />
+        <Route path='/sobre' element={<Sobre />} />
+      </Routes>
+    </Router>
   );
 }
+
 
 export default App;
---
+++ src/paginas/Preços/Preços.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import './Preços.css';
+
+export function Preços() {
+    return (
+        <h1>isso é a(o) Preço</h1>
+    )
+}
